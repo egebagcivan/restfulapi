ff338b23e564691f2e90be6fdda5b0413bd08f24
Ege <ege.bagcivan@mudimedia.comm>
2020-12-20 15:47:02 +0300

    readme degisti

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
 const movie = require('./routes/movie');
+
 
 const app = express();
 
@@ -19,7 +21,9 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- helper/db.js
+++ helper/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    mongoose.connect('mongodb+srv://movie_user:egecan3535@cluster0.fyus6.mongodb.net/admin?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.connect('mongodb+srv://movie_user:egecan3535@cluster0.fyus6.mongodb.net/movie-api', {useNewUrlParser: true, useUnifiedTopology: true});
     mongoose.connection.on('open', () => {
         console.log('MongoDB: Connected');
       });
       mongoose.connection.on('error', (err) => {
         console.log('MongoDB: Error', err);
       });
+
+      mongoose.Promise = global.Promise;
 };
